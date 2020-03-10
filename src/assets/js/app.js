94b15c6883b9e37838c0171a19c48d1b890a764f
Isaac Morrier <isaacmorrier@gmail.com>
2020-03-09 22:49:51 -0400

    add mailchimp form submission

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import 'what-input';
-import validate from 'jquery-validation';
-import 'jquery-modal';
+//import validate from 'jquery-validation';
+//import 'jquery-modal';
 
 // Foundation JS relies on a global varaible. In ES6, all imports are hoisted
 // to the top of the file so if we used`import` to import Foundation,
@@ -18,6 +18,9 @@ require('foundation-sites');
 $(document).ready( fn );
 
 function fn() {
+  
+  Foundation.Abide.defaults.patterns['phone'] = /^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$/;
+  Foundation.Abide.defaults.patterns['zip'] = /^\d{5}$/;
   
   $(document).foundation();
   
@@ -117,10 +120,59 @@ function fn() {
 ====================================== FORMS
 =================================== */
 
+$('.signup-opener').on('click', function() {
+  $(this).attr("data-origin", "true");
+  $.ajax('_signup.html').
+    done(function(content) {
+      $('#modal').html(content).foundation('open');
+  });
+});
+
+$('.contact-opener').on('click', function() {
+  $.ajax('_contact.html').
+    done(function(content) {
+      $('#modal').html(content).foundation('open');
+  });
+});
+
+$('[data-reveal]').on('open.zf.reveal', function () {
+  var options = {liveUpdate: true, validateOnBlur: true},
+      form = new Foundation.Abide($('form'), options);
+  });
   
-  
-  
-  // sign-up form
+  $(document).on("submit", function(ev) {
+    ev.preventDefault();
+    $.ajax({
+      url: '//localhost/dc-2026/dist/process.php',
+      type: 'POST',
+      data: $('#mc-embedded-subscribe-form').serialize(),
+      success: function(response) {
+        if(response === 200) {
+          $('#mc-embedded-subscribe-form').hide();
+          $('#signup-header').hide();
+          $('.signup-success').show();
+//            var height = $('.sign-up').height();
+//            $('.sign-up').height(height);
+//            $('.sign-up .form-controls').addClass('done');
+//            $('.sign-up .success-message').addClass('show');
+//            $('.sign-up .error-message').removeClass('show');
+        } else {
+//            $('.sign-up .error-message').addClass('show');
+        }
+      },
+      error: function(xhr, status, error) {
+        alert(xhr.responseText);
+        $('.signup-error').addClass('show');
+      }
+//      error: function() {
+//          window.alert('error');
+//          $('.sign-up .error-message').text('Error! Try again');
+//      }
+    });
+//    window.alert("Submit for form id "+ev.target.id+" intercepted");
+  });
+//  
+//  
 //  $('.sign-up').validate({
 //    highlight: function(element, errorClass) {
 //      $(element).parent().addClass(errorClass);
@@ -163,4 +215,14 @@ function fn() {
 //      }
 //    }
 //  });
+//  $('form').foundation('enableValidation');
+//  $(document).on("formvalid.zf.abide", function(ev,frm) {pushSubmit();});
+//  var dataLocation = $('[data-origin="true"]').data("location"); //grabs the location value from the button that originated the modal opening
+//  selectionListener();
+//  var selectedLocation = $("[data-location-option='" + dataLocation + "']");
+//  selectedLocation.prop("selected", "selected"); // set location to selected
+//  $('[data-origin="true"]').attr("data-origin", "false"); // reset originating button
+
+  
+  // sign-up form
 }
