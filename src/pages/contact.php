ec3dde02307b5a1ea5280c87f2c5f0c313e680ac
Isaac Morrier <isaacmorrier@gmail.com>
2020-03-16 09:49:59 -0400

    cleanup contact php

--- src/pages/contact.php
+++ src/pages/contact.php
@@ -12,22 +12,22 @@ $body = $_REQUEST['BODY'];
 
 try {
     // PROD Server settings 
-//    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-//    $mail->isSMTP();                                            // Send using SMTP
-//    $mail->Host       = 'localhost';                    // Set the SMTP server to send through
-//    $mail->SMTPAuth   = false;                                   // Enable SMTP authentication
-//    $mail->SMTPAutoTLS = false;
-//    $mail->Port       = 25;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+//    $mail->SMTPDebug = SMTP::DEBUG_SERVER
+//    $mail->isSMTP();
+//    $mail->Host       = 'localhost';
+//    $mail->SMTPAuth   = false;
+//    $mail->SMTPAutoTLS = false
+//    $mail->Port       = 25;
   
     //LOCAL Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'washingtondc2026@gmail.com';                     // SMTP username
-    $mail->Password   = '';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'washingtondc2026@gmail.com';
+    $mail->Password   = '';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
 
     //Recipients
     $mail->setFrom('webform@dc2026.org', 'dc2026.org Webform');
@@ -36,7 +36,7 @@ try {
 
 
     // Content
-    $mail->isHTML(false);                                  // Set email format to HTML
+    $mail->isHTML(false);
     $mail->Subject = 'DC2026 Contact Form Submission Attn: ' . $subject;
     $mail->Body    = 'New Form Submission' . PHP_EOL . PHP_EOL .
                      'Email: ' . $email . '' . PHP_EOL .
