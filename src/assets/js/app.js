e47a15befc0ced59381e0d9ec04a1e0d625a0c68
Isaac Morrier <isaacmorrier@gmail.com>
2020-10-05 10:44:13 -0400

    remove local Mail Chimp signup forms and add p2a links

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -136,23 +136,23 @@ function fn() {
 ====================================== FORMS
 =================================== */
 
-$('.signup-opener').on('click', function() {
-  $(this).attr("data-origin", "true");
-  $.ajax('_signup.html').
-    done(function(content) {
-      $('#modal').html(content).foundation('open');
-      var dataIntent = $('[data-origin="true"]').data("intent"); //grabs the intent value from the button that originated the modal opening
-      if ( dataIntent === 'volunteer') {
-				$('.pledge-content').addClass('hide');
-				$('.volunteer-content').removeClass('hide');
-        $('[data-receive="intent"]').prop("checked", true); // set volunteer checkbox to checked
-      } else {
-				$('.volunteer-content').addClass('hide');
-				$('.pledge-content').removeClass('hide');
-			}
-      $('[data-origin="true"]').attr("data-origin", "false"); // reset originating button
-  });
-});
+//$('.signup-opener').on('click', function() {
+//  $(this).attr("data-origin", "true");
+//  $.ajax('_signup.html').
+//    done(function(content) {
+//      $('#modal').html(content).foundation('open');
+//      var dataIntent = $('[data-origin="true"]').data("intent"); //grabs the intent value from the button that originated the modal opening
+//      if ( dataIntent === 'volunteer') {
+//				$('.pledge-content').addClass('hide');
+//				$('.volunteer-content').removeClass('hide');
+//        $('[data-receive="intent"]').prop("checked", true); // set volunteer checkbox to checked
+//      } else {
+//				$('.volunteer-content').addClass('hide');
+//				$('.pledge-content').removeClass('hide');
+//			}
+//      $('[data-origin="true"]').attr("data-origin", "false"); // reset originating button
+//  });
+//});
 
 $('.contact-opener').on('click', function() {
   $.ajax('_contact.html').
@@ -168,64 +168,64 @@ $('[data-reveal]').on('open.zf.reveal', function () {
   
   $(document).on("submit", function(ev) {
     ev.preventDefault();
-    if ( ev.target.id == 'mc-embedded-subscribe-form') {
-      var submitVal = $('#mc-embedded-subscribe').val();      
-      $.ajax({
-        url: '/process.php',
-        type: 'POST',
-        data: $('#mc-embedded-subscribe-form').serialize(),
-        beforeSend: function() {
-          $('#mc-embedded-subscribe').prop('disabled', true).val("Sending...");
-        },        
-        success: function(response, status, xhr) { 
-					console.log('xhr.responseText = '+xhr.responseText+ '\n' +
-											'xhr.status = '+xhr.status+ '\n' +
-											'xhr.getAllResponseHeaders() = '+xhr.getAllResponseHeaders()+ '\n' +
-											'status = '+status+ '\n' +
-											'response = '+response);
-          if(response == 200) {
-						console.log("200");
-            $('#mc-embedded-subscribe-form').hide();
-            $('#signup-header').hide();
-            $('.signup-error').hide();
-            $('.signup-success').show();
-          } else {
-						console.log("else");
-            $('.signup-error').show();
-						$('.error-paragraph').hide();
-						switch (response) {
-							case 0:
-								console.log("switch case 0")
-								$('#error-paragraph-connection').show();
-								break;
-							case 400:
-								console.log("switch case 400")
-								$('#error-paragraph-400').show();
-								break;
-							case 500:
-								console.log("switch case 500")
-								$('#error-paragraph-500').show();
-								break;
-							default:
-								$('#error-paragraph').show();
-						};
-					}
-        },
-        error: function(xhr, status, error) {
-          console.log('xhr.responseText = '+xhr.responseText+ '\n' +
-                      'xhr.status = '+xhr.status+ '\n' +
-                      'xhr.getAllResponseHeaders() = '+xhr.getAllResponseHeaders()+ '\n' +
-                      'status = '+status+ '\n' +
-                      'error = '+error);
-          $('.signup-error').show();
-					$('.error-paragraph').hide();
-					$('#error-paragraph-connection').show();
-        },
-        complete: function() {
-          $('#mc-embedded-subscribe').prop('disabled', false).val(submitVal);
-        }        
-      });
-    }
+//    if ( ev.target.id == 'mc-embedded-subscribe-form') {
+//      var submitVal = $('#mc-embedded-subscribe').val();      
+//      $.ajax({
+//        url: '/process.php',
+//        type: 'POST',
+//        data: $('#mc-embedded-subscribe-form').serialize(),
+//        beforeSend: function() {
+//          $('#mc-embedded-subscribe').prop('disabled', true).val("Sending...");
+//        },        
+//        success: function(response, status, xhr) { 
+//					console.log('xhr.responseText = '+xhr.responseText+ '\n' +
+//											'xhr.status = '+xhr.status+ '\n' +
+//											'xhr.getAllResponseHeaders() = '+xhr.getAllResponseHeaders()+ '\n' +
+//											'status = '+status+ '\n' +
+//											'response = '+response);
+//          if(response == 200) {
+//						console.log("200");
+//            $('#mc-embedded-subscribe-form').hide();
+//            $('#signup-header').hide();
+//            $('.signup-error').hide();
+//            $('.signup-success').show();
+//          } else {
+//						console.log("else");
+//            $('.signup-error').show();
+//						$('.error-paragraph').hide();
+//						switch (response) {
+//							case 0:
+////								console.log("switch case 0")
+//								$('#error-paragraph-connection').show();
+//								break;
+//							case 400:
+////								console.log("switch case 400")
+//								$('#error-paragraph-400').show();
+//								break;
+//							case 500:
+////								console.log("switch case 500")
+//								$('#error-paragraph-500').show();
+//								break;
+//							default:
+//								$('#error-paragraph').show();
+//						};
+//					}
+//        },
+//        error: function(xhr, status, error) {
+//          console.log('xhr.responseText = '+xhr.responseText+ '\n' +
+//                      'xhr.status = '+xhr.status+ '\n' +
+//                      'xhr.getAllResponseHeaders() = '+xhr.getAllResponseHeaders()+ '\n' +
+//                      'status = '+status+ '\n' +
+//                      'error = '+error);
+//          $('.signup-error').show();
+//					$('.error-paragraph').hide();
+//					$('#error-paragraph-connection').show();
+//        },
+//        complete: function() {
+//          $('#mc-embedded-subscribe').prop('disabled', false).val(submitVal);
+//        }        
+//      });
+//    }
     if ( ev.target.id == 'c-form') {
       var datap = $('#c-form').serialize(),
           submitVal = $('#c-SUBMIT').val();
